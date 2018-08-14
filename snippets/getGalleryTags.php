36217d61cb57cc7991ddd682f0931e4fbca78174
electrica <electricaev@gmail.com>
2018-08-14 10:43:16 +0600

    Photo Gallery

--- snippets/getGalleryTags.php
+++ snippets/getGalleryTags.php
@@ -1,3 +1,29 @@
 <?php
 
-return 'asdasd';
+
+$parents = 918;
+
+//Выбрать ресурсы с родителем фотогалереи
+$where = array('parent' => $parents);
+$resources = $modx->getIterator('modResource', $where);
+
+$resArr = array();
+foreach ($resources as $resource){
+    $resArr[] = $resource->get('id');
+}
+
+//Теперь выбираем все ТВ и группируем их
+
+$q = $modx->newQuery('modTemplateVarResource');
+$q->where(array(
+    'contentid:IN' => $resArr,
+    'tmplvarid' => 73
+));
+
+$tvs = $modx->getIterator('modTemplateVarResource', $q);
+$tvArr = array();
+foreach ($tvs as $tv) {
+    $tvArr[] = $tv->get('value');
+}
+
+print_r($tvArr);
